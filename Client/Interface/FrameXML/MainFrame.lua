ca7d10171088a132cc7aecf723bbb8d9545aa5a2
stoneharry <stoneharryjames@gmail.com>
2014-03-24 23:12:02 +0000

    Added header. Appears to work on widescreen resolutions (huh?).

--- Client/Interface/FrameXML/MainFrame.lua
+++ Client/Interface/FrameXML/MainFrame.lua
@@ -13,19 +13,41 @@ local vars = 1
 for value in string.gmatch(res, '([^x]+)') do
 	if vars == 1 then
 		model:SetWidth(value)
+		SCREEN_WIDTH = value
 		vars = nil
 	else
 		model:SetHeight(value)
+		SCREEN_HEIGHT = value
 	end
 end
 res = nil
 model:SetLight(1,0,0,-0.5,-0.5,0.7,1.0,1.0,1.0,0.8,1.0,1.0,0.8);
 
--- Set background model
 function mainFrameLoaded()
+	-- Set background model
 	model:SetModel("Interface\\Glues\\Models\\UI_Orc\\UI_Orc.m2");
+	-- Set frame levels
 	MainFrame_Back:SetFrameLevel(1)
 	MainFrame_Header:SetFrameLevel(1)
+	
+	--[[
+	local SCREEN_HEIGHT
+	local res = GetCVar("gxResolution")
+	local vars = 0
+	for value in string.gmatch(res, '([^x]+)') do
+		vars = vars + 1
+		if vars == 2 then
+			SCREEN_HEIGHT = tonumber(value)
+		end
+	end
+	
+	local scale = 1
+	if SCREEN_HEIGHT ~= 1080 then
+		Head01:SetScale(SCREEN_HEIGHT / 1080)
+		Head02:SetScale(SCREEN_HEIGHT / 1080)
+		Head03:SetScale(SCREEN_HEIGHT / 1080)
+	end
+	]]
 end
 
 function mainFrameUpdate()
