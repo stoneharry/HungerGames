bbaef0593f67697e791f6d67e75bd7ad7842b1b7
Terrorblade <derekccarp@gmail.com>
2014-04-08 16:45:54 -0400

    Fix HG_Game.cpp initializer.

--- Core/src/server/game/Battlegrounds/HG_Game.cpp
+++ Core/src/server/game/Battlegrounds/HG_Game.cpp
@@ -1,14 +1,13 @@
 
 #include "HG_Game.h"
 
-HG_Game::HG_Game(std::string gameName, Player* plr)
+HG_Game::HG_Game()
 {
 	GUID = ++CUR_GUID;
-	this->gameName = gameName;
 	inGame = false;
 	killMe = false;
 
-	playersInGame[0] = plr;
+	//playersInGame[0] = plr;
 	for (int i = 1; i < 10; ++i)
 		playersInGame[i] = NULL;
 }
