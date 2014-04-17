8a466901c74d939eb579bc0b93671f7b098b07a2
Stoneharry <stoneharryjames@gmail.com>
2014-04-17 19:24:43 +0100

    ...Actually GUID can be the primary key.

--- SQL/HG stats table.sql
+++ SQL/HG stats table.sql
@@ -2,11 +2,10 @@
 DROP TABLE IF EXISTS `character_hg`;
 
 CREATE TABLE `character_hg` (
-  `entry` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `guid` int(10) unsigned NOT NULL,
   `wins` int(10) unsigned NOT NULL DEFAULT '0',
   `losses` int(10) unsigned NOT NULL DEFAULT '0',
   `current_rank` int(10) unsigned NOT NULL DEFAULT '0',
   `highest_rank` int(10) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`entry`)
+  PRIMARY KEY (`guid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
